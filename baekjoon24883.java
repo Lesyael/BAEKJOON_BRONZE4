8c2db90a512a92aea2e3d8bc21c2a9c6270e0099
Lesyael <112386686+Lesyael@users.noreply.github.com>
2022-09-21 19:15:28 +0900

    220921

---
+++ baekjoon24883.java
@@ -0,0 +1,23 @@
+package bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon24883 {
+
+	public static void main (String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String n = br.readLine().toLowerCase();
+		
+		if (n.equals("n")) {
+			System.out.println("Naver D2");
+		} else {
+			System.out.println("Naver Whale");
+		}
+		
+		
+	}
+
+}
